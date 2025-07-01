ad98b2db6737df276c49ad8660f35f7f0c053087
BigGunBrother <nokelove1111@gmail.com>
2025-07-01 20:14:14 +0800

    fix: 調整.env 登入mongodb 設定

---
+++ server.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
