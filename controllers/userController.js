a27d8415e35f48b63151a02ea5a3c1654d7389ec
BigGunBrother <nokelove1111@gmail.com>
2025-07-23 15:46:59 +0800

    fix: 測試secure, sameSite

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,8 +49,8 @@ export const login = async (req, res, next) => {
     // // ✅ 設定 httpOnly Cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "development" ? false : true,
-      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 天
     });
 
