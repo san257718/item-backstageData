5f95b10523bb68f2ec15dcd2048fd4af5bffedee
BigGunBrother <nokelove1111@gmail.com>
2025-07-23 17:43:25 +0800

    fix: 修正userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,7 @@ import protect from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.get("/", protect, getUsers);
-router.get("/total_number_of_products", protect, getTotalNumberOfProducts); // ✅ 修正
+router.get("/total_number_of_products", getTotalNumberOfProducts); // ✅ 修正
 router.get("/order_price", protect, getOrderPrices); // ✅ 修正
 router.post("/", createUser);
 router.post("/login", login);
