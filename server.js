d2d351ebbd8abadb5f4bd4efe18b94cc81348290
Len <nokelove1111@gmail.com>
2026-03-20 14:16:00 +0800

    fix: 修正vercel

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ import connectDB from "./config/db.js";
 dotenv.config();
 connectDB();
 
-// if (process.env.NODE_ENV !== "production") {
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-// }
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 export default app; // 確保有 export default
