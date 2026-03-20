1f7bd100b3124b3e7f59e58c7ecaa0717c8a4f71
Len <nokelove1111@gmail.com>
2026-03-20 14:01:19 +0800

    fix: 修正vercel

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ import connectDB from "./config/db.js";
 dotenv.config();
 connectDB();
 
-const SERVER_PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-// const PROT = process.env.NODE_ENV_DEV;
-
-app.listen(SERVER_PORT, () => {
-  // 使用修正後的 SERVER_PORT
-  console.log(`✅ Server running on port ${SERVER_PORT} in ${process.env.NODE_ENV || 'development'} mode`);
-  // 如果你希望在後端日誌中顯示生產環境的 API URL，可以額外打印：
-  // if (process.env.NODE_ENV === 'production') {
-  //   console.log(`🚀 Production API URL: ${process.env.PROD_API_KEY}`);
-  // }
-});
+export default app; // 確保有 export default
